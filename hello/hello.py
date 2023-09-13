785b2fdfea596da788a118b2fc1964816b0d2601
Aashir Tuladhar <tuladhar002@gannon.edu>
2023-09-13 11:43:50 -0400

    added default value

--- hello/hello.py
+++ hello/hello.py
@@ -1,3 +1,11 @@
 import sys
 
-print("Hello {0}".format(sys.argv[1]))
+#default for no arg
+name = "World"
+
+#set name var for arg passed in
+if(len(sys.argv)>=2):
+    name = sys.argv[1]
+
+#print
+print("Hello {0}".format(name))
